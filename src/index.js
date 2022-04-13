c5eb14b2f0ec762c9fc74e794cca2a0a111cc849
Brian Jackson <brianjackson1224@gmail.com>
2022-04-13 15:25:21 -0700

    re-factor components

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
@@ -13,40 +13,31 @@ const Search = () => {
                 id="header-search"
                 placeholder="Mood"
                 name="s"
+                style={{marginRight: 10}}
             />
+            <button>Go!</button>
         </form>
     </div>
 }
 
-class Image extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            catImage: [
-                null
-                //'https://images.theconversation.com/files/443350/original/file-20220131-15-1ndq1m6.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C3354%2C2464&q=45&auto=format&w=926&fit=clip'
-            ],
-        };
-    }
-  
-    render() {
-        return (
-            <div className='image-container'>
-                <img src={this.state.catImage[0]} alt="Cat" />
-            </div>
-        );
-    }
+const Image = (props) => {
+    const [catImage, setCatImage] = useState('https://images.theconversation.com/files/443350/original/file-20220131-15-1ndq1m6.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C3354%2C2464&q=45&auto=format&w=926&fit=clip');
+
+    return (
+        catImage &&
+        <div className='image-container'>
+            <img src={catImage} alt="Cat" />
+        </div>
+    );
 }
   
-class Emoticats extends React.Component {
-    render() {
-        return (
-            <div className="container">
-                <Search />
-                <Image />
-            </div>
-        );
-    }
+const Emoticats = () => {
+    return (
+        <div className="container">
+            <Search />
+            <Image />
+        </div>
+    );
 }
   
   // ========================================
