7d8c2afa81491047eff4a28d9bb67476cfaf64c2
Brian Jackson <brianjackson1224@gmail.com>
2022-04-13 21:46:40 -0700

    Search submit and styled

--- src/index.js
+++ src/index.js
@@ -2,36 +2,60 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const Search = () => {
-    return <div className='search-container'>
-        <form action="/" method="get">
-            <label htmlFor="header-search" style={{marginRight: 10}}>
-                <span className="visually-hidden">How are you feeling today?</span>
-            </label>
-            <input
-                type="text"
-                id="header-search"
-                placeholder="Mood"
-                name="s"
-                style={{marginRight: 10}}
-            />
-            <button>Go!</button>
-        </form>
-    </div>
-}
-
-const Image = (props) => {
-    const [catImage, setCatImage] = useState('https://images.theconversation.com/files/443350/original/file-20220131-15-1ndq1m6.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C3354%2C2464&q=45&auto=format&w=926&fit=clip');
-
-    return (
-        catImage &&
-        <div className='image-container'>
-            <img src={catImage} alt="Cat" />
-        </div>
-    );
-}
-  
 const Emoticats = () => {
+    const [catImage, setCatImage] = useState('');
+
+    const Search = () => {
+        const [searchInput, setSearchInput] = useState('');
+    
+        function handleSubmit(event) {
+            console.log("Search submitted.");
+            console.log("Value: " + searchInput);
+            event.preventDefault();
+        }
+    
+        function handleChange(event) {
+            setSearchInput(event.target.value);
+        }
+    
+        return <div className='search-container'>
+            <form 
+                action="/" 
+                method="get"
+                onSubmit={handleSubmit}
+            >
+                <div className='text-and-search'>
+                    <label htmlFor="header-search" style={{marginBottom: 20}}>
+                        <span className="visually-hidden">How are you feeling today?</span>
+                    </label>
+                    <div className='bar-and-button'>
+                        <input
+                            type="text"
+                            id="header-search"
+                            placeholder="Mood"
+                            name="s"
+                            style={{marginRight: 10}}
+                            value={searchInput}
+                            onChange={handleChange}
+                        />
+                        <input 
+                            type="submit" value="Submit"
+                        />
+                    </div>
+                </div>
+            </form>
+        </div>
+    }
+    
+    const Image = (props) => {
+        return (
+            catImage &&
+            <div className='image-container'>
+                <img src={catImage} alt="Cat" />
+            </div>
+        );
+    }
+
     return (
         <div className="container">
             <Search />
